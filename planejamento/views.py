317c5e84f272ed86b6d4f12dd1f1ba3bdca0d7d8
Bruno Henrique <brunodorea@outlook.com.br>
2023-07-05 15:44:38 -0300

    ➕ build: Extrato - definir_planejamento

---
+++ planejamento/views.py
@@ -0,0 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.shortcuts import render
+from perfil.models import Categoria
+import json
+
+def definir_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'definir_planejamento.html', {'categorias': categorias})
+
+@csrf_exempt
+def update_valor_categoria(request, id):
+    novo_valor = json.load(request)['novo_valor']
+    categoria = Categoria.objects.get(id = id)
+    categoria.valor_planejamento = novo_valor
+
+    categoria.save()
+
+    return JsonResponse({'status': 'SUCESSO'})
